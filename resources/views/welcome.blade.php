3027fcc39b293f11af7b0d1cb4aa6929c0a71919
rahnik <rahnik14@gmail.com>
2020-09-11 11:36:44 +0300

    kek

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,19 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Books
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                @foreach($books as $book)
+                    <div>{{ $book->title }}</div>
+                @endforeach
             </div>
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $books = DB::table('books')->get();
+    return view('welcome',['books' => $books]);
 });
